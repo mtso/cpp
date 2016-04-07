b823f78b7efc569dc75aaa2797a2a5f6775e328f
mtso <mtso@business-cs.deanza.ad.fhda.edu>
2016-04-07 12:31:02 -0700

    First commit.

---
+++ CIS22A_MatthewTso_Spring2016/CIS22A_MatthewTso_Spring2016/MyHelloWorld.cpp
@@ -0,0 +1,15 @@
+// CIS22A : Matthew Tso
+// This is my first program
+// 
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World - Greetings from Matthew Tso";
+	cout << endl << endl << endl << endl << endl << endl;
+	system("pause");
+	return 0;
+}
